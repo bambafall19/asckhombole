c8ddfc89996f6bb63cdea4ad69b6c73d5f248eef
asc khombole <khomboleasc@gmail.com>
2026-01-06 17:56:58 +0000

    ## Error Type Recoverable Error
    ## Error Message
    Hydration failed becau

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -9,13 +9,11 @@ interface LogoProps {
 export function Logo({ logoUrl, className }: LogoProps) {
   return (
     <div className={`flex items-center gap-3 ${className}`}>
-       <div className="flex items-center justify-center">
-        {logoUrl ? (
-            <Image src={logoUrl} alt="Logo ASC Khombole" width={36} height={36} className="object-contain" />
-        ) : (
-            <SoccerBallIcon className="text-primary h-10 w-10" />
-        )}
-      </div>
+      {logoUrl ? (
+          <Image src={logoUrl} alt="Logo ASC Khombole" width={36} height={36} className="object-contain" />
+      ) : (
+          <SoccerBallIcon />
+      )}
       <span className="font-bold text-xl text-foreground font-headline">
         ASC Khombole
       </span>
